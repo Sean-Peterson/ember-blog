9e0461117e2b8e875ccff9e659f8b30677ff76dc
Brynna McCarter and Sean Peterson <brynna@>
2017-03-22 15:51:22 -0700

    add one to many relationship between posts and comments

--- app/routes/post.js
+++ app/routes/post.js
@@ -9,14 +9,24 @@ export default Ember.Route.extend({
       post.destroyRecord();
       this.transitionTo('index');
     },
-    update(rental, params) {
+    update(post, params) {
       Object.keys(params).forEach(function(key) {
         if(params[key]!==undefined) {
-          rental.set(key,params[key]);
+          post.set(key,params[key]);
         }
       });
-      rental.save();
+      post.save();
       this.transitionTo('index');
     },
+    saveComment(params) {
+      console.log(params.model);
+      var newComment = this.store.createRecord('comment', params);
+      var post = params.post;
+      post.get('comments').addObject(newComment);
+      newComment.save().then(function() {
+        return post.save();
+      });
+      this.transitionTo('post', post);
+    }
   }
 });
